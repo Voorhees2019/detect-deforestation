136eea9db367280d3d0f0d6c9067f9aed467ef47
Artur Petryk <petrikartur@gmail.com>
2021-05-16 14:32:27 +0300

    added ability to change own password

--- accounts/urls.py
+++ accounts/urls.py
@@ -21,5 +21,8 @@ urlpatterns = [
          name='password_reset_complete'),
     path('dashboard/', views.dashboard, name='dashboard'),
     # password change
-
+    path('password-change/', auth_views.PasswordChangeView.as_view(
+        template_name='accounts/password_change.html'), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='accounts/password_change_done.html'), name='password_change_done'),
 ]
